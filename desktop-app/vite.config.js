dff60120ab9319b292f5ac6114fccaec45265617
a58293 <kaisong0619@gmail.com>
2026-07-17 00:33:49 +0800

    1

--- desktop-app/vite.config.js
+++ desktop-app/vite.config.js
@@ -1,2 +1,19 @@
 import { defineConfig } from "vite";
-export default defineConfig({clearScreen:false,server:{port:1420,strictPort:true,host:"127.0.0.1"},envPrefix:["VITE_","TAURI_"],build:{target:"es2021",cssCodeSplit:true,sourcemap:false}});
+
+export default defineConfig({
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: "127.0.0.1",
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    target: "es2021",
+    cssCodeSplit: true,
+    sourcemap: false,
+  },
+});
